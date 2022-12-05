b2662b93c3eeb93b4367402f4218d3261f8e7f46
Mark Evans <mark.evans@kingfisher.com>
2022-12-05 17:03:36 +0000

    Create Day02.kt
    very bad day2 implementation

---
+++ src/Day02.kt
@@ -0,0 +1,91 @@
+enum class Hand {
+    Rock, Paper, Scissors
+}
+enum class CodedHand(val hand: Hand, val score: Int) {
+    A(Hand.Rock, 1),
+    B(Hand.Paper, 2),
+    C(Hand.Scissors, 3),
+    X(Hand.Rock, 1),
+    Y(Hand.Paper, 2),
+    Z(Hand.Scissors, 3),
+}
+
+enum class CodedHand2(val score: Int) {
+    A(1),
+    B(2),
+    C(3),
+    X(0),
+    Y(3),
+    Z(6),
+}
+fun main() {
+
+    fun getScore(input: List<String>): Int {
+        var sum = 0
+
+        input.forEach {
+                s ->
+            var opponent = CodedHand.valueOf(s[0].toString());
+            var me = CodedHand.valueOf(s[2].toString());
+
+            var pointsForMatch = 0
+            if(opponent.score - me.score == -1 || (opponent.hand == Hand.Scissors && me.hand == Hand.Rock)){
+                pointsForMatch = 6
+            }
+            else if(opponent.score == me.score){
+                pointsForMatch = 3;
+            }
+
+            sum += pointsForMatch + me.score;
+        }
+
+        return sum;
+    }
+
+    fun part1(input: List<String>): Int {
+
+        return getScore(input);
+    }
+
+    fun part2(input: List<String>): Int {
+        var sum = 0
+
+        input.forEach {
+                s ->
+            var opponent = CodedHand2.valueOf(s[0].toString());
+            var me = CodedHand2.valueOf(s[2].toString());
+
+            var pointsForItem = 0
+            if(me == CodedHand2.X){
+                pointsForItem = opponent.score -1;
+            }
+            else if(me == CodedHand2.Z){
+                pointsForItem = opponent.score +1;
+            }
+            else if(me == CodedHand2.Y){
+                pointsForItem = opponent.score;
+            }
+
+            if(pointsForItem == 0){
+                pointsForItem = 3;
+            }
+            if(pointsForItem == 4){
+                pointsForItem = 1;
+            }
+
+            sum += pointsForItem + me.score;
+        }
+
+        return sum;
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    println(part1(testInput))
+    println(part2(testInput))
+    check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
